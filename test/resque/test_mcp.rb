ac91c841969a5ccb3f36c83095d7f70e7f5a2053
Josch Bockler <9265647+jbockler@users.noreply.github.com>
2026-07-02 11:58:45 +0200

    initial commit

---
+++ test/resque/test_mcp.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Resque::TestMcp < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Resque::Mcp::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
